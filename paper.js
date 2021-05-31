12992000cfbb9a1811053f6ce261e624049e6291
Arthur2946 <71204899+Arthur2946@users.noreply.github.com>
2021-05-31 18:51:40 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -10,7 +10,7 @@ class Paper {
      //this.y = y;
      this.radius = 70;
      this.body = Bodies.circle(x,y, this.radius, options);
-     this.image = loadImage("sprites/paper.png");
+     this.image = loadImage("paper.png");
      World.add(world, this.body);
  }
    display() {
